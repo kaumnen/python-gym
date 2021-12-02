3ddb518dca2dcf26303a614ddb1aea82cf794641
komnen0v1c <main@komnen.xyz>
2021-12-02 14:01:08 +0100

    refactor main.py, more readable

--- AoC-2021/day 2/main.py
+++ AoC-2021/day 2/main.py
@@ -2,24 +2,18 @@ with open('input.txt', 'r') as f:
     data = f.read().splitlines()
 
 
-def star_one(data):
+def star_one(data : list) -> int:
     depth = 0
     horizontal_position = 0
     for line in data:
-
-        # only forward has an 'a' in it
-        if 'a' in line:
-            _, value = line.split(' ')
+        direction, value = line.split(' ')
+        if direction == 'forward':
             horizontal_position += int(value)
 
-        # only down has a 'n' in it
-        elif 'n' in line:
-            _, value = line.split(' ')
+        elif direction == 'down':
             depth += int(value)
 
-        # it has to be up in else case since there is neither 'a' or 'n' in 'up'
         else:
-            _, value = line.split(' ')
             depth -= int(value)
     return horizontal_position * depth
 
@@ -29,26 +23,20 @@ print(star_one(data))
 
 ################################################################
 
-def star_two(data):
+def star_two(data : list) -> int:
     depth = 0
     horizontal_position = 0
     aim = 0
     for line in data:
-
-        # only 'forward' has an 'a' in it
-        if 'a' in line:
-            _, value = line.split(' ')
+        direction, value = line.split(' ')
+        if direction == 'forward':
             horizontal_position += int(value)
             depth += aim * int(value)
 
-        # only 'down' has a 'n' in it
-        elif 'n' in line:
-            _, value = line.split(' ')
+        elif direction == 'down':
             aim += int(value)
 
-        # it has to be up in else case since there is neither 'a' or 'n' in 'up'
         else:
-            _, value = line.split(' ')
             aim -= int(value)
     return horizontal_position * depth
 
